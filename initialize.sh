bebad4ba977c6229dc78d41c7d453cd38d79f470
Siwat Sirichai <siwat2545@cud55.com>
2021-08-24 11:33:42 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,4 @@
-echo "To install, please enter the container console and run /installer/setup.sh"
+echo "To install, please enter the container console and run /installer/install.sh"
 echo "Please use /config as the install directory"
 /config/bin/multicraft start
 tail -f /dev/null
