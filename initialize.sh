9da9410f1a77277f7bb493a5129ac0855d144827
Siwat Sirichai <siwat2545@cud55.com>
2021-10-21 12:55:39 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,4 @@
 echo "To install, please enter the container console and run /installer/install.sh"
 echo "Please use /multicraft_daemon as the install directory"
-/multicraft_daemon/bin/multicraft start
+/multicraft_daemon/bin/multicraft -nv start
 tail -f /dev/null
